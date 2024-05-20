28db5eb844416610e3e3c542437a2916b2f3c972
nmcuan-hihi <nguyennnminhhhquannn@gmail.com>
2024-05-20 07:25:46 +0700

    demo api

--- app/Screen/TestScreen.jsx
+++ app/Screen/TestScreen.jsx
@@ -1,24 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Button, StyleSheet, FlatList, ScrollView } from 'react-native';
-import { Picker} from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
+import axios from 'axios';
 
 
 
 const TestScreen = () => {
   const [selectedValue, setSelectedValue] = useState();
+  const [arrEmployee, setArrEmployee] = useState([]);
+  useEffect(() => {
+    getArremployee();
+  }, []);
+
+  const getArremployee = async () => {
+    try {
+      const response = await axios.get('http://10.0.2.2:8080/employee/getAll');
+      setArrEmployee(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Error', 'Failed to fetch class list. Please try again!');
+    }
+  };
   return (
     <View style={styles.container}>
-     
+      {/* <Picker
+        selectedValue={selectedValue}
+        onValueChange={(itemvalue, index) =>
 
-    <Picker
-    selectedValue={selectedValue}
-    onValueChange={(itemvalue, index) =>
+          setSelectedValue(itemvalue)
+        }>
+        <Picker.Item label='adsfdsf' value="dsfasdf" />
+        <Picker.Item label='sdfgd' value="sdfd" />
+      </Picker> */}
 
-      setSelectedValue(itemvalue)
-    }>
-      <Picker.Item label='adsfdsf' value="dsfasdf" />
-      <Picker.Item label='sdfgd' value="sdfd" />
-    </Picker>
+
+      <Picker
+        selectedValue={selectedValue}
+        onValueChange={(itemvalue, index) =>
+          setSelectedValue(itemvalue)
+        }>
+        {arrEmployee.map((item) => (
+          <Picker.Item key={item.maNV} label={item.tenNV} value={item.maNV} />
+        ))}
+      </Picker>
     </View>
   );
 };
@@ -28,7 +53,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
   },
- 
+
 });
 
 export default TestScreen;
--- app/Screen/ViewAttendanceSheet.jsx
+++ app/Screen/ViewAttendanceSheet.jsx
@@ -1,5 +1,5 @@
 import { View, Text, Dimensions, ScrollView, FlatList, Pressable, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 import OnBack from '../Compoment/OnBack';
 import { AntDesign, Feather } from "@expo/vector-icons";
@@ -10,55 +10,57 @@ import { DataTable } from 'react-native-paper';
 import TimeSheet from '../Compoment/TimeSheet';
 import moment from 'moment';
 import SheetItem from '../Compoment/ItemSheet';
+import axios from 'axios';
 
 
 export default function ViewAttendanceSheet() {
-  //  const [currentDate, setCurrentDate] = useState(moment());
+    //  const [currentDate, setCurrentDate] = useState(moment());
     const [year, setYear] = useState(moment().year());
     const employeesData = arrEmployee.getAllEmployees();
     const nextYear = () => {
-      const nyear = year +1;
-      setYear(nyear);
-  };
-  const prevYear = () => {
-      const prYear = year -1;
-      setYear(prYear);
-  };
-  return (
-    <LinearGradient colors={["#7F7FD5", "#E9E4F0"]} style={{ width, height }}>
+        const nyear = year + 1;
+        setYear(nyear);
+    };
+    const prevYear = () => {
+        const prYear = year - 1;
+        setYear(prYear);
+    };
+    
+    return (
+        <LinearGradient colors={["#7F7FD5", "#E9E4F0"]} style={{ width, height }}>
 
-    <View>
-        <OnBack /> 
-        <Pressable>
-                <View
-                    style={{
-                        flexDirection: "row",alignItems: "center",gap: 10,marginLeft: "auto",marginRight: "auto",                        
-                    }}
-                >
-                    <AntDesign   
-                    onPress={prevYear}                    
-                        name="left"
-                        size={24}
-                        color="blue"
-                    />                  
-                        <Text style={{fontSize: 20}}>{year}</Text>                
-                    <AntDesign  
-                    onPress={nextYear}                    
-                        name="right"
-                        size={24}
-                        color="blue"
+            <View>
+                <OnBack />
+                <Pressable>
+                    <View
+                        style={{
+                            flexDirection: "row", alignItems: "center", gap: 10, marginLeft: "auto", marginRight: "auto",
+                        }}
+                    >
+                        <AntDesign
+                            onPress={prevYear}
+                            name="left"
+                            size={24}
+                            color="blue"
+                        />
+                        <Text style={{ fontSize: 20 }}>{year}</Text>
+                        <AntDesign
+                            onPress={nextYear}
+                            name="right"
+                            size={24}
+                            color="blue"
+                        />
+                    </View>
+                </Pressable>
+                <View style={{ padding: 20 }}>
+                    <FlatList
+                        data={employeesData}
+                        //renderItem={sheetItem}    
+                        renderItem={({ item }) => <SheetItem item={item} year={year} />}
                     />
                 </View>
-            </Pressable>
-       <View style={{padding: 20}}>      
-        <FlatList
-        data={employeesData} 
-         //renderItem={sheetItem}    
-         renderItem={({ item }) => <SheetItem item={item} year={year} />}
-        />
-       </View>     
-    </View>
-    </LinearGradient>
+            </View>
+        </LinearGradient>
 
-  )
+    )
 }
